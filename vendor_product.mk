2bc091ae46f79240f3ba968ca86fb0f50c4b2158
enesykaya <enesykaya@gmail.com>
2023-03-19 23:43:59 +0300

    usb: Update USB configuration
    * Guard usb props for newer targets
    
    * Set vendor.usb.controller based on platform
    
    Change-Id: Id0fc6d4f6976789539994ee2c64e023f002dd061

--- vendor_product.mk
+++ vendor_product.mk
@@ -2,19 +2,25 @@
 # Default property overrides for various function configurations
 # These can be further overridden at runtime in init*.rc files as needed
 #
+ifneq (,$(filter 5.10 5.15, $(TARGET_KERNEL_VERSION)))
 PRODUCT_PROPERTY_OVERRIDES += vendor.usb.rndis.func.name=gsi
 PRODUCT_PROPERTY_OVERRIDES += vendor.usb.rmnet.func.name=gsi
 PRODUCT_PROPERTY_OVERRIDES += vendor.usb.rmnet.inst.name=rmnet
 PRODUCT_PROPERTY_OVERRIDES += vendor.usb.dpl.inst.name=dpl
+endif
 
-ifneq ($(filter bengal_515 monaco,$(TARGET_BOARD_PLATFORM)),)
+ifneq ($(filter bengal bengal_515 holi monaco trinket,$(TARGET_BOARD_PLATFORM)),)
   PRODUCT_PROPERTY_OVERRIDES += vendor.usb.controller=4e00000.dwc3
-else
+else ifneq ($(filter atoll kalama kona lahaina lito msmnile parrot sdm710 sdm845 sm6150 taro,$(TARGET_BOARD_PLATFORM)),)
   PRODUCT_PROPERTY_OVERRIDES += vendor.usb.controller=a600000.dwc3
+else ifneq ($(filter msm8998 sdm660,$(TARGET_BOARD_PLATFORM)),)
+  PRODUCT_PROPERTY_OVERRIDES += vendor.usb.controller=a800000.dwc3
+else ifneq ($(filter msm8953,$(TARGET_BOARD_PLATFORM)),)
+  PRODUCT_PROPERTY_OVERRIDES += vendor.usb.controller=7000000.dwc3
 endif
 
 # QDSS uses SW path on these targets
-ifneq ($(filter lahaina taro bengal_515 kalama monaco,$(TARGET_BOARD_PLATFORM)),)
+ifneq ($(filter  bengal_515 kalama lahaina monaco taro,$(TARGET_BOARD_PLATFORM)),)
   PRODUCT_PROPERTY_OVERRIDES += vendor.usb.qdss.inst.name=qdss_sw
 else
   PRODUCT_PROPERTY_OVERRIDES += vendor.usb.qdss.inst.name=qdss
