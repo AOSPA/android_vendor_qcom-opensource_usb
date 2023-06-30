fd7f9ee63498fbfaff9e83768527d46b2dd73778
chrisl7 <wandersonrodriguesf1@gmail.com>
2023-06-30 14:26:02 +0000

    USB: Create condition to check kernel version for bengal and kona
    [1] - 5.15 kernel time
    
    Change-Id: Ie6630a03b9e51e8ccb84ff81b483426f7f7f22c0
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- vendor_product.mk
+++ vendor_product.mk
@@ -7,10 +7,14 @@ PRODUCT_PROPERTY_OVERRIDES += vendor.usb.rmnet.func.name=gsi
 PRODUCT_PROPERTY_OVERRIDES += vendor.usb.rmnet.inst.name=rmnet
 PRODUCT_PROPERTY_OVERRIDES += vendor.usb.dpl.inst.name=dpl
 
-ifneq ($(filter bengal_515 monaco,$(TARGET_BOARD_PLATFORM)),)
+ifeq ($(TARGET_KERNEL_VERSION),5.15)
+ifneq ($(filter bengal monaco,$(TARGET_BOARD_PLATFORM)),)
   PRODUCT_PROPERTY_OVERRIDES += vendor.usb.controller=4e00000.dwc3
   PRODUCT_PROPERTY_OVERRIDES += ro.boot.usb.dwc3_msm=4e00000.ssusb
-else ifneq ($(filter msm8998 sdm660,$(TARGET_BOARD_PLATFORM)),)
+endif
+endif
+
+ifneq ($(filter msm8998 sdm660,$(TARGET_BOARD_PLATFORM)),)
   PRODUCT_PROPERTY_OVERRIDES += vendor.usb.controller=a800000.dwc3
 else ifneq ($(filter msm8953,$(TARGET_BOARD_PLATFORM)),)
   PRODUCT_PROPERTY_OVERRIDES += vendor.usb.controller=7000000.dwc3
@@ -19,10 +23,14 @@ else ifeq ($(filter msm8937,$(TARGET_BOARD_PLATFORM)),)
 endif
 
 # QDSS uses SW path on these targets
-ifneq ($(filter lahaina taro bengal_515 kalama monaco kona,$(TARGET_BOARD_PLATFORM)),)
+ifeq ($(TARGET_KERNEL_VERSION),5.15)
+ifneq ($(filter bengal kona,$(TARGET_BOARD_PLATFORM)),)
+  PRODUCT_PROPERTY_OVERRIDES += vendor.usb.qdss.inst.name=qdss_sw
+else ifneq ($(filter lahaina taro kalama monaco,$(TARGET_BOARD_PLATFORM)),)
   PRODUCT_PROPERTY_OVERRIDES += vendor.usb.qdss.inst.name=qdss_sw
 else
   PRODUCT_PROPERTY_OVERRIDES += vendor.usb.qdss.inst.name=qdss
+endif
 endif
 
 ifeq ($(TARGET_HAS_DIAG_ROUTER),true)
