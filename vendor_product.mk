7506a71830dcab22255e6f2ce3d76c36884f03d2
Ke Du <quic_kedu@quicinc.com>
2023-07-20 13:43:20 +0530

    usb: Enable USB configurations for trinket
    Use software path for USB QDSS data transfer and
    enable USBGadget HAL for trinket. Update the value
    of vendor.usb.controller being used based on targets.
    
    Change-Id: I7a0080bcc919e34567a3d5b42cf3659ada625535

--- vendor_product.mk
+++ vendor_product.mk
@@ -7,7 +7,7 @@ PRODUCT_PROPERTY_OVERRIDES += vendor.usb.rmnet.func.name=gsi
 PRODUCT_PROPERTY_OVERRIDES += vendor.usb.rmnet.inst.name=rmnet
 PRODUCT_PROPERTY_OVERRIDES += vendor.usb.dpl.inst.name=dpl
 
-ifneq ($(filter bengal monaco pitti,$(TARGET_BOARD_PLATFORM)),)
+ifneq ($(filter bengal monaco trinket pitti,$(TARGET_BOARD_PLATFORM)),)
   PRODUCT_PROPERTY_OVERRIDES += vendor.usb.controller=4e00000.dwc3
   PRODUCT_PROPERTY_OVERRIDES += ro.boot.usb.dwc3_msm=4e00000.ssusb
 else ifneq ($(filter msm8998 sdm660,$(TARGET_BOARD_PLATFORM)),)
@@ -21,7 +21,7 @@ else
 endif
 
 # QDSS uses SW path on these targets
-ifneq ($(filter bengal kona lahaina taro blair kalama pineapple sun parrot monaco tuna kera,$(TARGET_BOARD_PLATFORM)),)
+ifneq ($(filter bengal kona lahaina taro blair kalama pineapple sun parrot monaco trinket tuna kera,$(TARGET_BOARD_PLATFORM)),)
   PRODUCT_PROPERTY_OVERRIDES += vendor.usb.qdss.inst.name=qdss_sw
 else
   PRODUCT_PROPERTY_OVERRIDES += vendor.usb.qdss.inst.name=qdss
