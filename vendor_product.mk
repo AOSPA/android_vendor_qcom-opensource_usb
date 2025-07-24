37cff8256aee6f8df498ae7a91bbc4a190caf0a0
Omkar Chandorkar <gotenksIN@aospa.co>
2025-07-24 13:30:29 +0000

    Revert "usb: Import missing platforms definitions from kailua"
    This reverts commit 84ee0583979e93b3609eb976d700f705fc4cdb09.
    
    Reason for revert: Picking changes from quic
    
    Change-Id: Iccfe795cb0df567bb9f0deb2e8f1a4e9c6c47b36

--- vendor_product.mk
+++ vendor_product.mk
@@ -7,13 +7,8 @@ PRODUCT_PROPERTY_OVERRIDES += vendor.usb.rmnet.func.name=gsi
 PRODUCT_PROPERTY_OVERRIDES += vendor.usb.rmnet.inst.name=rmnet
 PRODUCT_PROPERTY_OVERRIDES += vendor.usb.dpl.inst.name=dpl
 
-ifneq ($(filter bengal blair monaco trinket,$(TARGET_BOARD_PLATFORM)),)
+ifneq ($(filter blair monaco,$(TARGET_BOARD_PLATFORM)),)
   PRODUCT_PROPERTY_OVERRIDES += vendor.usb.controller=4e00000.dwc3
-  ifneq ($(filter bengal trinket,$(TARGET_BOARD_PLATFORM)),)
-    PRODUCT_SYSTEM_PROPERTIES += ro.boot.usb.dwc3_msm=4e00000.ssusb
-  else
-    PRODUCT_SYSTEM_PROPERTIES += ro.boot.usb.dwc3_msm=4e00000.hsusb
-  endif
 else ifneq ($(filter msm8998 sdm660,$(TARGET_BOARD_PLATFORM)),)
   PRODUCT_PROPERTY_OVERRIDES += vendor.usb.controller=a800000.dwc3
 else ifneq ($(filter msm8953,$(TARGET_BOARD_PLATFORM)),)
@@ -25,7 +20,7 @@ else
 endif
 
 # QDSS uses SW path on these targets
-ifneq ($(filter lahaina taro bengal blair kalama pineapple sun parrot monaco tuna kera trinket,$(TARGET_BOARD_PLATFORM)),)
+ifneq ($(filter lahaina taro blair kalama pineapple sun parrot monaco tuna kera,$(TARGET_BOARD_PLATFORM)),)
   PRODUCT_PROPERTY_OVERRIDES += vendor.usb.qdss.inst.name=qdss_sw
 else
   PRODUCT_PROPERTY_OVERRIDES += vendor.usb.qdss.inst.name=qdss
